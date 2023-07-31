d30de127f82e14b5c1d1ed3fd7b5a257a1cc3231
Michael <mckchan13@gmail.com>
2023-07-30 21:27:44 -0700

    made a change to retest new permissions on aws

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I made a change!!</p>
+        <p>I made another change!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
